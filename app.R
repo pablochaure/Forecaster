cdee7182b26f3641e7dc43ab2a8a2fc38fc35074
Nami Sunami <naoyuki.sunami@gmail.com>
2022-03-15 17:19:33 -0400

    Move the content of the `global.R` file to the app

--- app.R
+++ app.R
@@ -1,5 +1,24 @@
 ########### FORECASTER APP #################
 
+# Previous content in the R Global file:
+if (!Sys.info()[['user']] == 'RA135GG'){
+  # When running on shinyapps.io, create a virtualenv 
+  envs<-reticulate::virtualenv_list()
+  if(!'forecaster_app' %in% envs)
+  {
+    reticulate::virtualenv_create(envname = 'forecaster_app', 
+                                  python  = 'python3')
+    reticulate::virtualenv_install('forecaster_app', 
+                                   packages = c('numpy',
+                                                'python-xbrl',
+                                                'pandas',
+                                                'loguru',
+                                                'xmltodict',
+                                                'xlsxwriter',
+                                                'bs4'))
+  }
+}
+
 # LIBRARIES ----
 source(file = "00_scripts/libraries.R")
 libraries()
